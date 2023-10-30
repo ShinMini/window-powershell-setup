a302f27127d8132471b68203b17952905ecfbf55
ShinMini <gusals121234@gmail.com>
2023-10-30 16:31:52 +0900

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,71 +1,58 @@
-# window power shell profile ps1 writed by mini
+# This PowerShell profile is authored by mini
+# 실행 권한이 없다면 아래 주석을 해제하고 실행해주세요
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Confirm
 
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -> 권한이 없을 경우, 주석을 제거하고 실행해주세요
-
-# 모듈 불러오기
-Import-Module -Name Terminal-Icons # 터미널 아이콘을 표시해주는 모듈입니다표
-Import-Module -Name posh-git # 하단 status 창에 git branch 정보를 표시해주는 모듈입니다.
-Import-Module PSReadLine # 자동완성 기능을 활성화해주는 모듈입니다.
-
-# Set-PSReadLineOption -PredictionSource History # 자동완성 기능을 설정 명령어입니다.
-Set-PoshPrompt -Theme jandedobbeleer
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -EditMode Windows
-
-Invoke-Expression -Command $(gh completion -s powershell | Out-String)
-
-# 테마를 설정하는 명령어입니다. - 취향에 맞게 테마를 설정해주세요.
-# 테마 목록은, https://ohmyposh.dev/docs/themes 에서 확인할 수 있습니다.
-# 테마 변경을 원하시면, 아래 주석을 해제하고, 테마 이름을 변경해주세요. 현재 설정된 테마 이름 : [paradox]
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression 
-
-# 관리자 모드로 파워쉘을 실행하는 함수입니다. 
+# 관리자 권한으로 PowerShell을 실행하는 함수
 function admin {
    start-process powershell -verb runAs
-   Write-Output "				[ADMIN MODE] START "
+   Write-Host "		[ADMIN MODE] START "
 }
 
-# chrome을 실행하는 함수입니다. 
+# Chrome 브라우저를 실행하는 함수
 function chrome { 
-   Push-Location .
-   Write-Output "		[OPEN chrome] "
-   Set-Location "C:\Program Files\Google\Chrome\Application"
-   .\chrome.exe
-   Pop-Location
+   if (Test-Path "C:\Program Files\Google\Chrome\Application\chrome.exe") {
+      Push-Location .
+      Write-Host "		[OPEN Chrome] "
+      Set-Location "C:\Program Files\Google\Chrome\Application"
+      .\chrome.exe
+      Pop-Location
+   } else {
+      Write-Host "Chrome not found!"
+   }
 }
 
-## c++ 자동 빌더 & 실행 함수 -> 해당 함수를 실행하기 위해서는, build_file.cpp 파일이 존재해야합니다.
+# C++ 코드를 빌드하고 실행하는 함수
 function build {
    param (
       [string]$build_file
    )
-   g++ -g .\${build_file}.cpp -o .\${build_file}.exe
-   Write-Output " [파일 빌더 완료] "
-   invoke-item ".\${build_file}.exe"
+   if (Test-Command "g++") {
+      g++ -g .\${build_file}.cpp -o .\${build_file}.exe
+      Write-Host " [파일 빌드 완료] "
+      invoke-item ".\${build_file}.exe"
+   } else {
+      Write-Host "g++ not found!"
+   }
 }
 
-
-## Set-Alias -> alias를 설정하는 명령어입니다. (단축키를 설정하는 명령어입니다)
+# 별칭 설정 (Alias 설정)
 Set-Alias note notepad.exe
 Set-Alias touch New-Item
 Set-Alias rename Rename-Item
 
-## make loading bar status(when starting updating) -> 업데이트를 시작할 때, 로딩바를 표시해주는 함수입니다.
-for ($I = 1; $I -lt 101; $I++ ) {
+# 업데이트 시작 시 로딩바를 표시
+# 이 부분은 예시입니다. 실제 업데이트 코드와 연동 필요
+for ($I = 1; $I -lt 101; $I++) {
    Write-Progress -Activity Updating -Status 'Progress->' -PercentComplete $I -CurrentOperation OuterLoop
-   for ($j = 1; $j -lt 101; $j++ ) {
+   for ($j = 1; $j -lt 101; $j++) {
       Write-Progress -Id 1 -Activity Updating -Status 'Progress' -PercentComplete $j -CurrentOperation InnerLoop
    }
 }
 
-# profile set up이 종료되었을 때 표시됩니다. 
-Write-Output "
-  			[load done]		
-"
+# 프로파일 설정이 완료되었을 때 출력됩니다.
+Write-Host "		[Load Done] "
 
-# 만일 powershell이 chocolatey library 환경변수 위치를 파악하지 못하는 경우, 아래 주석을 해제해주세요.
-
+# Chocolatey 라이브러리 환경변수 설정 (필요하다면 주석 해제)
 # $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 # if (Test-Path($ChocolateyProfile)) {
 #   Import-Module "$ChocolateyProfile"
